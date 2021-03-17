d71aa95e39943ea73a1a97222bf12726de1aaaef
ahmedibrahiim287 <49068254+ahmedibrahiim287@users.noreply.github.com>
2021-03-17 15:54:40 +0200

    nav bar without pic

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,70 +1,75 @@
 import React, { Component } from "react";
+import logo from "../"
 
 export class Navigation extends Component {
   render() {
     return (
-      <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+      <nav id="menu" className="navbar navbar-default navbar-fixed-top navbar-dark">
         <div className="container">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle collapsed"
-              data-toggle="collapse"
-              data-target="#bs-example-navbar-collapse-1"
-            >
-              {" "}
-              <span className="sr-only">Toggle navigation</span>{" "}
-              <span className="icon-bar"></span>{" "}
-              <span className="icon-bar"></span>{" "}
-              <span className="icon-bar"></span>{" "}
-            </button>
-            <a className="navbar-brand page-scroll" href="#page-top">
-landing page
-         </a>{" "}
-          </div>
-
-          <div
-            className="collapse navbar-collapse"
-            id="bs-example-navbar-collapse-1"
-          >
-            <ul className="nav navbar-nav navbar-right">
+         
+        <div className="navbar-header naaz">
+        <button
+          type="button"
+          className="navbar-toggle collapsed"
+          data-toggle="collapse"
+          data-target="#bs-example-navbar-collapse-1"
+        >
+          {" "}
+          <span className="sr-only">Toggle navigation</span>{" "}
+          <span className="icon-bar"></span>{" "}
+          <span className="icon-bar"></span>{" "}
+          <span className="icon-bar"></span>{" "}
+        </button>
+        <a className="navbar-brand page-scroll" href="#page-top">
+        qsdasd
+      </a>{" "}
+      </div>
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+           
+            <ul className="d-md-none nav navbar-nav navbar-right">
               <li>
+
+              </li>
+            </ul>
+
+
+            <ul className="nav navbar-nav navbar-left">
+            <li>
                 <a href="#features" className="page-scroll">
-                  Features
+                      Home
                 </a>
               </li>
+              <li>
+                <a href="#testimonials" className="page-scroll">
+                      Attend
+                </a>
+              </li>
+              <li>
+                <a href="#services" className="page-scroll">
+                      Watch
+                </a>
+              </li>
+              </ul>
+              <ul className="nav navbar-nav navbar-right">
               <li>
                 <a href="#about" className="page-scroll">
                   About
                 </a>
               </li>
               <li>
-                <a href="#services" className="page-scroll">
-                  Services
-                </a>
-              </li>
-              <li>
-                <a href="#portfolio" className="page-scroll">
-                  Gallery
-                </a>
-              </li>
-              <li>
-                <a href="#testimonials" className="page-scroll">
-                  Testimonials
-                </a>
-              </li>
-              <li>
                 <a href="#team" className="page-scroll">
-                  Team
+                Sponsors
                 </a>
               </li>
               <li>
                 <a href="#contact" className="page-scroll">
-                  Contact
+                Contact us 
                 </a>
               </li>
             </ul>
+
           </div>
+      
         </div>
       </nav>
     );
